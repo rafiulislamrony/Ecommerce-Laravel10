1ced3a19df42beb833117773bde2f65148db2507
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-08-12 01:28:04 +0600

    Product details page content show

--- app/Http/Controllers/ProductDetailsController.php
+++ app/Http/Controllers/ProductDetailsController.php
@@ -13,14 +13,14 @@ class ProductDetailsController extends Controller
         ->join('categories', 'products.category_id', 'categories.id')
         ->join('subcategories', 'products.subcategory_id', 'subcategories.id')
         ->join('brands', 'products.brand_id', 'brands.id')
-        ->select('products.*','categories.category_name', 'subcategories.subcategory_name', 'brands.brand_name	')
+        ->select('products.*','categories.category_name', 'subcategories.subcategory_name', 'brands.brand_name')
         ->where('products.id', $id)
         ->first();
 
         $color = $product->product_color;
-        $product_color = explode('',$color);
+        $product_color = explode(',',$color);
         $size = $product->product_size;
-        $product_size = explode('',$size);
+        $product_size = explode(',',$size);
 
         return view('pages.product_details',compact('product', 'product_color', 'product_size'));
     }
--- resources/views/pages/product_details.blade.php
+++ resources/views/pages/product_details.blade.php
@@ -11,29 +11,34 @@
             <!-- Images -->
             <div class="col-lg-2 order-lg-1 order-2">
                 <ul class="image_list">
-                    <li data-image="{{ asset('frontend/images/single_4.jpg') }}"><img src="{{ asset('frontend/images/single_4.jpg') }}" alt=""></li>
-                    <li data-image="{{ asset('frontend/images/single_2.jpg') }}"><img src="{{ asset('frontend/images/single_2.jpg') }}" alt=""></li>
-                    <li data-image="{{ asset('frontend/images/single_3.jpg') }}"><img src="{{ asset('frontend/images/single_3.jpg') }}" alt=""></li>
+                    <li data-image="{{ asset($product->image_one) }}"><img src="{{ asset($product->image_one) }}"
+                            alt=""></li>
+                    <li data-image="{{ asset($product->image_two) }}"><img src="{{ asset($product->image_two) }}"
+                            alt=""></li>
+                    <li data-image="{{ asset($product->image_three) }}"><img src="{{ asset($product->image_three) }}"
+                            alt=""></li>
                 </ul>
             </div>
 
             <!-- Selected Image -->
             <div class="col-lg-5 order-lg-2 order-1">
-                <div class="image_selected"><img src="{{ asset('frontend/images/single_4.jpg') }}" alt=""></div>
+                <div class="image_selected"><img src="{{ asset($product->image_one) }}" alt=""></div>
             </div>
 
             <!-- Description -->
             <div class="col-lg-5 order-3">
+
                 <div class="product_description">
-                    <div class="product_category">Laptops</div>
-                    <div class="product_name">MacBook Air 13</div>
+                    <div class="product_category">{{ $product->category_name }} > {{ $product->subcategory_name }}
+                    </div>
+                    <div class="product_name">{{ $product->product_name }}</div>
                     <div class="rating_r rating_r_4 product_rating">
                         <i></i><i></i><i></i><i></i><i></i>
                     </div>
                     <div class="product_text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum. laoreet
-                            turpis, nec sollicitudin dolor cursus at. Maecenas aliquet, dolor a faucibus
-                            efficitur, nisi tellus cursus urna, eget dictum lacus turpis.</p>
+                        <p>
+                            {!! str_limit($product->product_details, $limit = 1200) !!}
+                        </p>
                     </div>
                     <div class="order_info d-flex flex-row">
                         <form action="#">
@@ -77,7 +82,18 @@
 
                             </div>
 
-                            <div class="product_price">$2000</div>
+                            <div class="product_price">
+                                @if($product->discount_price == NULL)
+                                <span>${{ $product->selling_price }}</span>
+                                @else
+                                ${{ $product->discount_price }}<span style="text-decoration: line-through;">${{
+                                    $product->selling_price
+                                    }}</span>
+                                @endif
+
+                            </div>
+
+
                             <div class="button_container">
                                 <button type="button" class="button cart_button">Add to Cart</button>
                                 <div class="product_fav"><i class="fas fa-heart"></i></div>
@@ -99,115 +115,44 @@
         <div class="row">
             <div class="col">
                 <div class="viewed_title_container">
-                    <h3 class="viewed_title">Recently Viewed</h3>
-                    <div class="viewed_nav_container">
-                        <div class="viewed_nav viewed_prev"><i class="fas fa-chevron-left"></i></div>
-                        <div class="viewed_nav viewed_next"><i class="fas fa-chevron-right"></i></div>
-                    </div>
+                    <h3 class="viewed_title">Product Details</h3>
                 </div>
 
                 <div class="viewed_slider_container">
-
-                    <!-- Recently Viewed Slider -->
-
-                    <div class="owl-carousel owl-theme viewed_slider">
-
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_1.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$225<span>$300</span></div>
-                                    <div class="viewed_name"><a href="#">Beoplay H7</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button class="nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home"
+                                type="button" role="tab" aria-controls="nav-home" aria-selected="true">Product Details</button>
+                            <button class="nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile"
+                                type="button" role="tab" aria-controls="nav-profile"
+                                aria-selected="false">Video Link</button>
+                            <button class="nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact"
+                                type="button" role="tab" aria-controls="nav-contact"
+                                aria-selected="false">Product Review</button>
                         </div>
-
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_2.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$379</div>
-                                    <div class="viewed_name"><a href="#">LUNA Smartphone</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
+                            aria-labelledby="nav-home-tab">
+                            <br>
+                            {!! $product->product_details !!}
                         </div>
-
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_3.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$225</div>
-                                    <div class="viewed_name"><a href="#">Samsung J730F...</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
+                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <br>
+                            {{ $product->video_link }}
                         </div>
+                        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                          <br>
+                          What is business insurance and how does it work?
+There are lots of risks involved in running a business. To protect against this risk, business owners take out business insurance policies in the event that something happens.
 
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item is_new d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_4.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$379</div>
-                                    <div class="viewed_name"><a href="#">Huawei MediaPad...</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
-                        </div>
+There are many different types of business insurance policies, but depending on what policy you have, business insurance can cover your business for lost income, legal liability, damage to property, and more.
 
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_5.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$225<span>$300</span></div>
-                                    <div class="viewed_name"><a href="#">Sony PS4 Slim</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <!-- Recently Viewed Item -->
-                        <div class="owl-item">
-                            <div
-                                class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-                                <div class="viewed_image"><img src="images/view_6.jpg" alt=""></div>
-                                <div class="viewed_content text-center">
-                                    <div class="viewed_price">$375</div>
-                                    <div class="viewed_name"><a href="#">Speedlink...</a></div>
-                                </div>
-                                <ul class="item_marks">
-                                    <li class="item_mark item_discount">-25%</li>
-                                    <li class="item_mark item_new">new</li>
-                                </ul>
-                            </div>
+Some types of business insurance are compulsory, either by law (such as worker’s compensation insurance) or because those you deal with may require it.
                         </div>
                     </div>
+
+
 
                 </div>
             </div>
