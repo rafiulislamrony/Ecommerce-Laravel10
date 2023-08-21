b3062a1c9aeeec44764d1c2db4849b2214c5480c
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-08-21 06:34:37 +0600

    Update Payment and order code

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -19,112 +19,99 @@ class PaymentController extends Controller
 
     public function payment(Request $request)
     {
+        try {
+            if ($request->payment == 'stripe') {
+                $settings = DB::table('settings')->first();
+                $shipping_charge = $settings->shipping_charge;
+                $vat = $settings->vat;
 
-        if ($request->payment == 'stripe') {
-            // Get Shopping Charge And vat
-            $settings = DB::table('settings')->first();
-            $shipping_charge = $settings->shipping_charge;
-            $vat = $settings->vat;
-            // Get Session Cart And cart Total
-            $cartTotal = 0;
-            if (Session::has('cart')) {
-                $cart = Session::get('cart');
-                if ($cart) {
-                    foreach ($cart as $product) {
-                        $cartTotal += (double) $product['price'] * (int) $product['qty'];
-                    }
+                $cartTotal = 0;
+                $cart = Session::get('cart') ?: [];
+
+                foreach ($cart as $product) {
+                    $cartTotal += (double) $product['price'] * (int) $product['qty'];
                 }
-            } else {
-                $cart = [];
-            }
-            // Get Carttotal if have Coupon
-            if (Session::has('coupon')) {
-                $subtotal = $cartTotal - Session::get('coupon')['discount'];
-            } else {
-                $subtotal = $cartTotal;
-            }
 
-            $totalAmount = Session::get('totalamount')['amount'];
-            $totalAmount = (int) $totalAmount;
+                $subtotal = $cartTotal - (Session::has('coupon') ? Session::get('coupon')['discount'] : 0);
+                $totalAmount = (int) Session::get('totalamount')['amount'];
 
+                Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+                $charge = Stripe\Charge::create([
+                    "amount" => $totalAmount * 100,
+                    "currency" => "usd",
+                    "source" => $request->stripeToken,
+                    "description" => "BlackBox Ecommerce.",
+                    "metadata" => ['order_id'=> uniqid()],
+                ]);
 
-            Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-            $charge = Stripe\Charge::create([
-                "amount" => $totalAmount * 100,
-                "currency" => "usd",
-                "source" => $request->stripeToken,
-                "description" => "BlackBox Ecommerce.",
-                "metadata" => ['order_id'=> uniqid()],
-            ]);
+                $user = Auth::id();
+                $order_id = DB::table('orders')->insertGetId([
+                    'user_id' => $user,
+                    'payment_id' => $charge->payment_method,
+                    'paying_amount' => number_format($charge->amount / 100, 2, '.', ','),
+                    'blnc_transection' => $charge->balance_transaction,
+                    'stripe_order_id' => $charge->metadata->order_id,
+                    'subtotal' => $subtotal,
+                    'shipping' => $shipping_charge,
+                    'vat' => $vat,
+                    'total' => $totalAmount,
+                    'status' => 0,
+                    'date' => date('d-m-y'),
+                    'month' => date('F'),
+                    'year' => date('Y'),
+                ]);
 
-            // Insert Data In orders Table
-            $user = Auth::id();
-            $data = array();
-            $data['user_id'] = $user;
-            $data['payment_id'] = $charge->payment_method;
-            $data['paying_amount'] = number_format($charge->amount / 100, 2, '.', ',');
-            $data['blnc_transection'] = $charge->balance_transaction;
-            $data['stripe_order_id'] = $charge->metadata->order_id;
-            $data['subtotal'] = $subtotal;
-            $data['shipping'] = $shipping_charge;
-            $data['vat'] = $vat;
-            $data['total'] = $totalAmount;
-            $data['status'] = 0;
-            $data['date'] = date('d-m-y');
-            $data['month'] = date('F');
-            $data['year'] = date('Y');
-            $order_id = DB::table('orders')->insertGetId($data);
+                $shipping = [
+                    'order_id' => $order_id,
+                    'ship_name' => $request->name,
+                    'ship_phone' => $request->phone,
+                    'ship_email' => $request->email,
+                    'ship_address' => $request->address,
+                    'ship_city' => $request->city,
+                    'ship_zip' => $request->zip,
+                ];
+                DB::table('shipping')->insert($shipping);
 
-            // Insert Data In Shipping Table
-            $shipping = array();
-            $shipping['order_id'] = $order_id;
-            $shipping['ship_name'] = $request->name;
-            $shipping['ship_phone'] = $request->phone;
-            $shipping['ship_email'] = $request->email;
-            $shipping['ship_address'] = $request->address;
-            $shipping['ship_city'] = $request->city;
-            $shipping['ship_zip'] = $request->zip;
-            DB::table('shipping')->insert($shipping);
-
-            // Insert Data In orders Table
-            $cart = Session::get('cart');
-            $details = array();
-            if ($cart) {
+                $details = [];
                 foreach ($cart as $row) {
-                    $details['order_id'] = $order_id;
-                    $details['product_id'] = $row['id'];
-                    $details['product_name'] = $row['name'];
-                    $details['color'] = $row['color'];
-                    $details['size'] = $row['size'];
-                    $details['quantity'] = $row['qty'];
-                    $details['singpleprice'] = $row['price'];
-                    $details['totalprice'] = $row['qty'] * $row['price'];
-                    DB::table('orders_details')->insert($details);
+                    $details[] = [
+                        'order_id' => $order_id,
+                        'product_id' => $row['id'],
+                        'product_name' => $row['name'],
+                        'color' => $row['color'],
+                        'size' => $row['size'],
+                        'quantity' => $row['qty'],
+                        'singpleprice' => $row['price'],
+                        'totalprice' => $row['qty'] * $row['price'],
+                    ];
                 }
-            }
+                DB::table('orders_details')->insert($details);
 
-            Session::forget('cart');
-            if(Session::has('coupon')){
-                Session::forget('coupon');
-            }
+                Session::forget('cart');
+                if (Session::has('coupon')) {
+                    Session::forget('coupon');
+                }
 
+                $notification = [
+                    'message' => 'Payment and Order Successfully Done.',
+                    'alert-type' => 'success',
+                ];
+                return redirect()->to('/')->with($notification);
+            } elseif ($request->payment == 'paypal') {
+                // PayPal handling code
+            } elseif ($request->payment == 'oncash') {
+                // On Cash handling code
+            } else {
+                echo "Cash On Delivery";
+            }
+        } catch (\Exception $e) {
             $notification = [
-                'message' => 'Payment and Order Successfully Done.',
-                'alert-type' => 'success',
+                'message' => 'An error occurred while processing your payment.',
+                'alert-type' => 'error',
             ];
-            return redirect()->to('/')->with($notification);
-
-        } elseif ($request->payment == 'paypal') {
-
-            # code...
-        } elseif ($request->payment == 'oncash') {
-
-            // return view('pages.payment.oncash', compact('data'));
-
-        } else {
-
-            echo "Cash On Delivery";
+            return redirect()->back()->with($notification);
         }
     }
+
 
 }
