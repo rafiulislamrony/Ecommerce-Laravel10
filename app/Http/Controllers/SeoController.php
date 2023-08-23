20658f07853ef21fda01609ef595bc76385feadb
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-08-23 19:26:39 +0600

    Seo update

--- app/Http/Controllers/SeoController.php
+++ app/Http/Controllers/SeoController.php
@@ -4,32 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class SeoController extends Controller
 {
     //
-    public function getSeo(){
-       $seo = DB::table('seo')->first();
-       return view('admin.seo.seo', compact('seo'));
+    public function getSeo()
+    {
+        try {
+            $seo = DB::table('seofd')->first();
+            return view('admin.seo.seo', compact('seo'));
+        } catch (\Exception $e) {
+
+            $notification = [
+                'message' => 'Error: ' . $e->getMessage(),
+                'alert-type' => 'error',
+            ];
+
+            return redirect()->back()->with($notification);
+        }
+    } 
+
+    public function updateSeo(Request $request) {
+        try {
+            $data = [
+                'meta_title' => $request->meta_title,
+                'meta_aurthor' => $request->meta_aurthor,
+                'meta_tag' => $request->meta_tag,
+                'meta_description' => $request->meta_description,
+                'google_analytics' => $request->google_analytics,
+                'bing_analytics' => $request->bing_analytics
+            ];
+
+            DB::table('seo')->where('id', $request->id)->update($data);
+
+            return redirect()->back()->with([
+                'message' => 'Seo Update Successfully.',
+                'alert-type' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->back()->with([
+                'message' => 'Error: ' . $e->getMessage(),
+                'alert-type' => 'error'
+            ]);
+        }
     }
 
-    public function updateSeo(Request $request){
-
-        $id = $request->id;
-
-        $data = array();
-        $data['meta_title'] = $request->meta_title;
-        $data['meta_aurthor'] = $request->meta_aurthor;
-        $data['meta_tag'] = $request->meta_tag;
-        $data['meta_description'] = $request->meta_description;
-        $data['google_analytics'] = $request->google_analytics;
-        $data['bing_analytics'] = $request->bing_analytics;
-
-        DB::table('seo')->where('id', $id)->update($data);
-
-        $notification = [
-            'message' => 'Seo Update Successfully.',
-            'alert-type' => 'success',
-        ];
-        return redirect()->back()->with($notification);
-    }
 }
